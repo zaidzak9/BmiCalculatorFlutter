45ef4ede4b07d0404c954f5fa1e8345f87a66a69
ZaidZakir <zaidzakir@hlbb.hongleong.com.my>
2021-06-23 13:09:56 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BMIcalculator());
+}
+
+class BMIcalculator extends StatefulWidget {
+  const BMIcalculator({Key key}) : super(key: key);
+
+  @override
+  _BMIcalculatorState createState() => _BMIcalculatorState();
+}
+
+class _BMIcalculatorState extends State<BMIcalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
