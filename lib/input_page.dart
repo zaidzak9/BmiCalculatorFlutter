bd50e44cd8df98d6786424bc288e1bb2133c8b1d
ZaidZakir <zaidzakir@hlbb.hongleong.com.my>
2021-06-25 21:17:01 +0800

    small changes to button color change

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,14 +1,29 @@
 import 'package:bmi_calculator_flutter/reusableUi/box_container.dart';
 import 'package:flutter/material.dart';
 
-class InputPage extends StatefulWidget {
-  const InputPage({Key key}) : super(key: key);
+const activeColor = Color(0xFF1D111);
+const inactiveColor = Color(0xFF1D1E33);
 
+class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
+  Color femaleColor = inactiveColor;
+  Color maleColor = inactiveColor;
+
+  void updateColor(int tap){
+    if(tap == 1){
+      maleColor = activeColor;
+      femaleColor = inactiveColor;
+    }else{
+      maleColor = inactiveColor;
+      femaleColor = activeColor;
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,23 +36,72 @@ class _InputPageState extends State<InputPage> {
                 child: Row(
               children: <Widget>[
                 Expanded(
-                    child: BoxContainer("male")
-                ),
+                    child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      updateColor(1);
+                    });
+                  },
+                  child: Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        color: maleColor,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: BoxContainer("male")),
+                )),
                 Expanded(
-                    child: BoxContainer("female")
-                )
+                    child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      updateColor(2);
+                    });
+                  },
+                  child: Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        color: femaleColor,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: BoxContainer("female")),
+                ))
               ],
             )),
             Expanded(
-                child: BoxContainer("progress")
+              child: Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: BoxContainer("progress")),
             ),
             Expanded(
                 child: Row(
               children: <Widget>[
                 Expanded(
-                    child: BoxContainer("weight")),
+                  child: Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF1D1E33),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: BoxContainer("weight")),
+                ),
                 Expanded(
-                    child: BoxContainer("height"))
+                  child: Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF1D1E33),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: BoxContainer("height")),
+                )
               ],
             )),
             Container(
--- lib/reusableUi/box_container.dart
+++ lib/reusableUi/box_container.dart
@@ -6,14 +6,7 @@ class BoxContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: double.infinity,
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: widgetToDisplay());
+    return widgetToDisplay();
   }
 
   BoxContainer(this.whichBox);
